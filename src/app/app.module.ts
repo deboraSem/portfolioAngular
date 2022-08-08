72b7c737e7802f500a73735cd979ef64e324992c
deboraSem <d.semichuch@gmail.com>
2022-08-08 19:19:02 -0300

    correccion declaracion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { SeccionesComponent } from './componentes/secciones/secciones.component';
 import { FooterComponent } from './componentes/footer/footer.component';
-import { LogoArgProgComponent } from './componentes/logo-arg-prog/logo-arg-prog.component';
-import { ComponentesComponent } from './head/componentes/componentes.component';
 import { HeadComponent } from './componentes/head/head.component';
 
 @NgModule({
@@ -16,8 +14,6 @@ import { HeadComponent } from './componentes/head/head.component';
     HeaderComponent,
     SeccionesComponent,
     FooterComponent,
-    LogoArgProgComponent,
-    ComponentesComponent,
     HeadComponent
   ],
   imports: [
