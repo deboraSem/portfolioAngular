82c5d6df9206a67f4d9cd28291b92c5c2ccef7c4
deboraSem <d.semichuch@gmail.com>
2022-09-06 16:05:19 -0300

    correcciones de estilos

---
+++ BACKEND/src/main/java/com/proyectoportfolio/DeboraS/DeboraSApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoportfolio.DeboraS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeboraSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeboraSApplication.class, args);
+	}
+
+}
---
+++ BACKEND/src/test/java/com/proyectoportfolio/DeboraS/DeboraSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoportfolio.DeboraS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeboraSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
